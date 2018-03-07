a70c80d6ac5682a70e055ce4a02bb1c5a0da1ded
Zdenek Kamensky <kamenskyla@gmail.com>
2018-03-07 15:10:40 +0100

    Vytvoreni adresaru Implementation a Interface a vytvoreni IEntity a presunuti EntityBase do adresare Implementation

--- iw5-2018-team20/Entities/Base/Implementation/EntityBase.cs
+++ iw5-2018-team20/Entities/Base/Implementation/EntityBase.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iw5_2018_team20.Entities.Base.Interfce;
 
 namespace iw5_2018_team20.Entities
 {
-    public abstract class EntityBase
+    public abstract class EntityBase : IEntity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
     }
---
+++ iw5-2018-team20/Entities/Base/Interface/IEntity.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace iw5_2018_team20.Entities.Base.Interface
+{
+    public interface IEntity
+    {
+        Guid Id { get; }
+    }
+}
