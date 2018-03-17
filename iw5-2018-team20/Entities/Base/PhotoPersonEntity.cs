b7347cb1cfcd800ea7ed00d6deaf667370330f8d
Vladimir Dusek <v.dusek96@gmail.com>
2018-03-17 17:35:29 +0100

    PhotoPersonEntity created

---
+++ iw5-2018-team20/Entities/Base/PhotoPersonEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using iw5_2018_team20.Entities.Base.Implementation;
+
+namespace iw5_2018_team20.Entities.Base
+{
+    public class PhotoPersonEntity : EntityBase
+    {
+        [Required]
+        public int positionX { get; set; }
+        public int positionY { get; set; }
+    }
+}
