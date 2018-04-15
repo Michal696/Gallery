3b71e414bfe692620e85ecc8e36ba51c5ca400fc
AIM666 <30591067+AIM666@users.noreply.github.com>
2018-04-15 11:28:56 +0200

    RelayCommand PhotoListViewModel SelectedPhotoMessage

---
+++ iw5-2018-team20/Commands/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace iw5_2018_team20.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> executeAction;
+        private readonly Func<object, bool> canExecuteAction;
+
+        public RelayCommand(Action<object> executeAction, Func<object, bool> canExecuteAction = null)
+        {
+            this.executeAction = executeAction;
+            this.canExecuteAction = canExecuteAction;
+        }
+
+        public RelayCommand(Action executeAction, Func<bool> canExecuteAction = null)
+            : this(p => executeAction(), p => canExecuteAction?.Invoke() ?? true)
+        {
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecuteAction?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            executeAction?.Invoke(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
