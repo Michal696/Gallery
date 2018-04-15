1cddb1bd681da1b8752d96fc1d769d9959f4b64e
AIM666 <30591067+AIM666@users.noreply.github.com>
2018-04-15 15:21:57 +0200

    +AlbumDetailViewModel +SaveAlbumCommand +DeleteMessage +NewMessage +UpdatedAlbumMessage

---
+++ iw5-2018-team20.BL/Messages/DeleteMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iw5_2018_team20.BL.Messages
+{
+    public class DeleteMessage
+    {
+        public DeleteMessage(Guid recipeId)
+        {
+            RecipeId = recipeId;
+        }
+
+        public Guid RecipeId { get; set; }
+    }
+}
