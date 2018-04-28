6bb76d82f6222da9947071e88c3c976e07ae37d3
AIM666 <30591067+AIM666@users.noreply.github.com>
2018-04-28 18:37:06 +0200

    MainViewModel

--- iw5-2018-team20/ViewModels/MainViewModel.cs
+++ iw5-2018-team20/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using iw5_2018_team20.BL;
 using iw5_2018_team20.BL.Messages;
 using iw5_2018_team20.Commands;
+using iw5_2018_team20.BL.Models;
+using iw5_2018_team20.BL.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,19 +12,24 @@ using System.Windows.Input;
 
 namespace iw5_2018_team20.ViewModels
 {
-    class MainViewModel
+    class MainViewModel : ViewModelBase
     {
         private readonly IMessenger messenger;
 
-        public string Name { get; set; } = "Nenacteno";
+        public string Name { get; set; } = "not_inserted";
+        public ICommand CreatePhotoCommand { get; set; }
         public ICommand CreatePersonCommand { get; set; }
 
         public MainViewModel(IMessenger messenger)
         {
             this.messenger = messenger;
+            CreatePhotoCommand = new RelayCommand(AddNewPhoto);
             CreatePersonCommand = new RelayCommand(AddNewPerson);
         }
-
+        private void AddNewPhoto()
+        {
+            messenger.Send(new NewMessage());
+        }
         private void AddNewPerson()
         {
             messenger.Send(new NewMessage());
