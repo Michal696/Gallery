63145af4ecac92bf1070f91e90eb75a73f5fdc2a
AIM666 <30591067+AIM666@users.noreply.github.com>
2018-05-01 15:17:10 +0200

    AddPerson

--- iw5-2018-team20/ViewModels/PersonListViewModel.cs
+++ iw5-2018-team20/ViewModels/PersonListViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 using iw5_2018_team20.BL;
 using iw5_2018_team20.BL.Messages;
 using iw5_2018_team20.BL.Models;
 using iw5_2018_team20.BL.Repositories;
 using iw5_2018_team20.Commands;
+using iw5_2018_team20.DAL.Entities;
 
 namespace iw5_2018_team20.ViewModels
 {
@@ -17,18 +20,72 @@ namespace iw5_2018_team20.ViewModels
     {
         private readonly PersonRepository personRepository;
         private readonly IMessenger messenger;
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
 
+        private PersonListModel selectedPerson; //= new PersonListModel();
 
         public ObservableCollection<PersonListModel> Persons { get; set; } = new ObservableCollection<PersonListModel>();
 
+        
         public ICommand SelectPersonCommand { get; }
+        public ICommand AddPersonCommand { get; }
+        public ICommand DeletePersonCommand { get; }
+
+        //public AddNewPersonInListCommand AddNewPerson { get; }
 
         public PersonListViewModel(PersonRepository personRepository, IMessenger messenger)
         {
+
+            Firstname = "Zadaj prve meno";
+            Surname = "Zadaj prezvisko";
+
+            //personRepository = new PersonRepository();
+
+            List<PersonListModel> persons = personRepository.GetAll();
+
             this.personRepository = personRepository;
             this.messenger = messenger;
 
-            SelectPersonCommand = new RelayCommand(PersonSelectionChanged);
+
+            SelectPersonCommand = new RelayCommand(SelectPersonInList);
+            AddPersonCommand = new RelayCommand(AddPersonInList);
+            //DeletePersonCommand = new RelayCommand(DeletePersonInList);
+
+            this.messenger.Register<DeletePersonInListMessage>(Reload);
+
+            //AddNewPerson = new AddNewPersonInListCommand(this, personRepository, selectedPerson, messenger);
+
+            //this.messenger.Register<NewPersonMessage>(NewPersonMessageReceived);
+            //this.messenger.Register<SelectedMessage>(SelectedPerson);
+            //this.messenger.Register<UpdatePersonListMessage>(UpdatePersonList);
+        
+            //OnLoad();
+        }
+
+        private void SelectPersonInList(object parameter)
+            {
+            if (parameter is PersonListModel person)
+            {
+                messenger.Send(new SelectedPersonMessage { Id = person.Id });
+            }
+
+        }
+
+        void AddPersonInList()
+        {
+            var person = new PersonListModel();
+            person.Firstname = Firstname;
+            person.Surname = Surname;
+            personRepository.Insert(person);
+            OnLoad();
+        }
+
+
+
+        public void Reload(DeletePersonInListMessage m)
+        {
+            OnLoad();
         }
 
         public void OnLoad()
@@ -41,15 +98,31 @@ namespace iw5_2018_team20.ViewModels
                 Persons.Add(person);
             }
         }
+        //void DeletePersonInList()///////////
+        //{
+        //    if (selectedPerson != null)
+        //    {
+        //        personRepository.Remove(selectedPerson.Id);
+        //        messenger.Send(new DeletePersonInListMessage()
+        //        {
+        //            Id = selectedPerson.Id
+        //        });
+        //    }
+        //    else
+        //        Console.WriteLine("No person is selected.");
 
-        private void PersonSelectionChanged(object parameter)
-        {
-            if (parameter is PersonListModel person)
-            {
-                messenger.Send(new SelectedMessage { Id = person.Id });
-            }
+        //    OnLoad();
+        //}
+        //private void PersonSelectionChanged(object parameter)
+        //{
+        //    if (parameter is PersonListModel person)
+        //    {
+        //        messenger.Send(new SelectedMessage { Id = person.Id });
+        //    }
 
-        }
+        //}
+
+
 
     }
 }
