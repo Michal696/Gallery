2bff429906f787b3d27dd4742e48f642626fe7cf
Zdenek Kamensky <kamenskyla@gmail.com>
2018-05-01 15:47:12 +0200

    Uprava fotek - poznamka a album

--- iw5-2018-team20/ViewModels/PhotoDetailViewModel.cs
+++ iw5-2018-team20/ViewModels/PhotoDetailViewModel.cs
@@ -16,9 +16,13 @@ namespace iw5_2018_team20.ViewModels
     public class PhotoDetailViewModel : ViewModelBase, INotifyPropertyChanged
     {
         private readonly PhotoRepository photoRepository;
+        private readonly AlbumRepository albumRepository;
         private readonly IMessenger messenger;
         private PhotoDetailModel detail;
 
+        public ObservableCollection<AlbumsListModel> AllAlbums { get; set; } = new ObservableCollection<AlbumsListModel>();
+
+        public Guid SelectedObject { get; set; }
         public string PoznamkaInput { get; set; }
 
 
@@ -36,14 +40,30 @@ namespace iw5_2018_team20.ViewModels
 
         public ICommand SavePhotoCommand { get; set; }
         public ICommand DeletePhotoCommand { get; set; }
+        public ICommand UpdatePhotoCommand { get; set; }
 
         public PhotoDetailViewModel(PhotoRepository photoRepository, IMessenger messenger)
         {
+            albumRepository = new AlbumRepository();
+
+            List<AlbumsListModel> albums = albumRepository.GetAll();
+
+            foreach (var albumListModel in albums)
+            {
+                AllAlbums.Add(new AlbumsListModel()
+                {
+                    Id = albumListModel.Id,
+                    Name = albumListModel.Name
+                });
+            }
+
+
             this.photoRepository = photoRepository;
             this.messenger = messenger;
 
             SavePhotoCommand = new SavePhotoCommand(photoRepository, this, messenger);
             DeletePhotoCommand = new RelayCommand(DeletePhoto);
+            UpdatePhotoCommand = new RelayCommand(UpdatePhoto);
 
             this.messenger.Register<SelectedMessage>(SelectedPhoto);
             this.messenger.Register<NewMessage>(NewPhotoMessageReceived);
@@ -59,6 +79,20 @@ namespace iw5_2018_team20.ViewModels
                 photoRepository.Remove(photoId);
                 messenger.Send(new DeletePhotoMessage(photoId));
             }
+        }
+
+
+        private void UpdatePhoto()
+        {
+            Mapper mapper = new Mapper();
+            if (SelectedObject != Guid.Empty)
+            {
+                Detail.Album = mapper.MapAlbumDetailModelToAlbumEntity(albumRepository.FindById(SelectedObject));
+                var album = albumRepository.FindById(SelectedObject);
+                album.Photos.Add(mapper.MapPhotoDetailModelToPhotoEntity(Detail));
+            }
+            Detail.Note = PoznamkaInput;
+            photoRepository.Update(Detail);
         }
 
         private void NewPhotoMessageReceived(NewMessage obj)
